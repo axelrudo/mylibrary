3766c4dbc36873874565b437e034d2d9e3625e87
Axel Pérez Gaspar <axel@rudo.es>
2024-07-02 14:32:11 +0200

    first commit

---
+++ Sources/MyLibrary/MyLibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  MyLibraryView.swift
+//  
+//
+//  Created by Axel Pérez Gaspar on 2/7/24.
+//
+
+import SwiftUI
+
+struct MyLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyLibraryView()
+}
